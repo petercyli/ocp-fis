1047fc5ceecc2443565c52315822a6d26db59c5a
sadhch <sadhana.chandravanka@feisystems.com>
2018-01-04 03:08:36 -0500

    Add FHIR Client related code
    #17177 - As a user, I can see a list of patients in the OCP, so that I know all of the patients in the system.

---
+++ ocp/src/main/java/gov/samhsa/ocp/ocp/config/OcpProperties.java
@@ -0,0 +1,42 @@
+package gov.samhsa.ocp.ocp.config;
+
+import ca.uhn.fhir.rest.api.EncodingEnum;
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Configuration
+@ConfigurationProperties(prefix = "ocp")
+@Data
+public class OcpProperties {
+
+    @NotNull
+    @Valid
+    private Fhir fhir;
+
+    @Data
+    public static class Fhir {
+
+        private Publish publish;
+
+        @Data
+        static class Publish {
+            @NotBlank
+            private ServerUrl serverUrl;
+            @NotBlank
+            private String clientSocketTimeoutInMs;
+            @NotNull
+            private EncodingEnum encoding = EncodingEnum.JSON;
+        }
+    }
+
+    @Data
+    static class ServerUrl {
+        @NotBlank
+        private String resource;
+    }
+}
