c6d74ef4f1b0d1f6f6445d6e3fabd1b5f2709ec9
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-05 16:15:33 -0500

    Create a controller for Practitioner                                                                                                                        #17176-As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PractitionerController.java
@@ -0,0 +1,24 @@
+package gov.samhsa.ocp.ocpfis.web;
+
+import gov.samhsa.ocp.ocpfis.service.PractitionerService;
+import gov.samhsa.ocp.ocpfis.service.dto.PractitionerDto;
+import org.hl7.fhir.dstu3.model.Practitioner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class PractitionerController {
+
+    @Autowired
+    private PractitionerService practitionerService;
+
+    @RequestMapping("/practitioners")
+    public List<PractitionerDto> getPractitioners(){
+        return practitionerService.readPractitioners();
+    }
+}
