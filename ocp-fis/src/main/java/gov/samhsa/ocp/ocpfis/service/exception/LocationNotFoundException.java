54eaa8fa06c07eb657d32d9528cae513342f0bc7
Shruti Rao <shruti.rao@feisystems.com>
2018-01-08 15:08:43 -0500

    Remove unused constructors
    #17170:As a user, I can see a list of locations in the OCP, so that I know all of the locations in the system.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/exception/LocationNotFoundException.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/exception/LocationNotFoundException.java
@@ -8,21 +8,7 @@ public class LocationNotFoundException extends RuntimeException {
     public LocationNotFoundException() {
         super();
     }
-
-    public LocationNotFoundException(String message, Throwable cause,
-                                     boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
-    public LocationNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public LocationNotFoundException(String message) {
         super(message);
-    }
-
-    public LocationNotFoundException(Throwable cause) {
-        super(cause);
     }
 }
