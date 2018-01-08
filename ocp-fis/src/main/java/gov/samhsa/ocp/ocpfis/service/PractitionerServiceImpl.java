562b24759e17e0becbec1e338a75423efeb99485
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-08 16:54:38 -0500

    Use modelmapper to map practitioner into practitionerDto                                                                                                                                                                        #17176-As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
@@ -6,42 +6,34 @@ import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.validation.FhirValidator;
 
 import gov.samhsa.ocp.ocpfis.config.OcpProperties;
+import gov.samhsa.ocp.ocpfis.service.dto.LocationDto;
 import gov.samhsa.ocp.ocpfis.service.dto.PractitionerDto;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.dstu3.model.Practitioner;
 import org.hl7.fhir.dstu3.model.Resource;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class PractitionerServiceImpl implements  PractitionerService{
 
-    private FhirContext fhirContext;
+    private final ModelMapper modelMapper;
 
-    private Map<Class<? extends Resource>, IGenericClient> fhirClients;
-
-    private IParser fhirJsonParser;
-
-    private FhirValidator fhirValidator;
-
-    private OcpProperties ocpProperties;
-
-    private IGenericClient practitionerFhirClient;
+    private final IGenericClient fhirClient;
 
     @Autowired
-    public PractitionerServiceImpl(FhirContext fhirContext, FhirValidator fhirValidator, Map<Class<? extends Resource>, IGenericClient> fhirClients, OcpProperties ocpProperties) {
-        this.fhirContext = fhirContext;
-        this.fhirValidator = fhirValidator;
-        this.fhirClients = fhirClients;
-        this.ocpProperties = ocpProperties;
-        this.practitionerFhirClient = fhirClients.getOrDefault(Practitioner.class, fhirClients.get(Resource.class));
+    public PractitionerServiceImpl(ModelMapper modelMapper, IGenericClient fhirClient) {
+        this.modelMapper = modelMapper;
+        this.fhirClient = fhirClient;
     }
 
     @Override
@@ -49,20 +41,14 @@ public class PractitionerServiceImpl implements  PractitionerService{
 
         List<PractitionerDto> list=new ArrayList<>();
 
-        Bundle bundle = practitionerFhirClient.search().forResource(Practitioner.class)
+        Bundle bundle = fhirClient.search().forResource(Practitioner.class)
                 .returnBundle(Bundle.class)
                 .execute();
 
-        for(int i=0;i<bundle.getEntry().size();i++) {
-            Practitioner practitioner = (Practitioner) bundle.getEntry().get(i).getResource();
+        List<Bundle.BundleEntryComponent> retrievedPractitioners = bundle.getEntry();
 
-            PractitionerDto practitionerDto=new PractitionerDto();
-            practitionerDto.setFamilyName((practitioner.getName().isEmpty()) ?"": practitioner.getName().get(0).getFamily());
-
-            list.add(practitionerDto);
-        }
-
-        return list;
+        return retrievedPractitioners.stream().map(retrievedPractitioner -> modelMapper.map(retrievedPractitioner.getResource(), PractitionerDto.class)).collect(Collectors.toList());
 
     }
+
 }
