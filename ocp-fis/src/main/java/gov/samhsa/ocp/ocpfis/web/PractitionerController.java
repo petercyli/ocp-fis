a5f65305a9efc8584377b345529bab402c43747b
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-10 11:02:47 -0500

    Add search Practitioner in controller.
    #17176-As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PractitionerController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PractitionerController.java
@@ -5,20 +5,25 @@ import gov.samhsa.ocp.ocpfis.service.dto.PractitionerDto;
 import org.hl7.fhir.dstu3.model.Practitioner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/practitioners")
 public class PractitionerController {
 
     @Autowired
     private PractitionerService practitionerService;
 
-    @RequestMapping("/practitioners")
+    @GetMapping
     public List<PractitionerDto> getPractitioners(){
         return practitionerService.getAllPractitioners();
     }
+
+    @GetMapping("/search")
+    public List<PractitionerDto> searchPractitioners(@RequestParam String searchValue){return practitionerService.searchPractitioners(searchValue);}
 }
