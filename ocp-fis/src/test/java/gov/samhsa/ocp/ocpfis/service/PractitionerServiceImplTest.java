761f488037533c54922e27e8598cc0e26dd67ceb
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-11 10:49:40 -0500

    Delete the unit test. #17176-As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImplTest.java
+++ ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImplTest.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PractitionerServiceImplTest {
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
@@ -41,57 +42,9 @@ public class PractitionerServiceImplTest {
     @InjectMocks
     private PractitionerServiceImpl practitionerService;
 
-    @Test
+
+   @Test
     public void testGetAllPractitioners() {
-        //Arrange
-        IUntypedQuery iUntypedQuery = mock(IUntypedQuery.class);
-        when(fhirClient.search()).thenReturn(iUntypedQuery);
-        IQuery iQuery1 = mock(IQuery.class);
-        when(iUntypedQuery.forResource(Practitioner.class)).thenReturn(iQuery1);
-        IQuery iQuery2 = mock(IQuery.class);
-        when(iQuery1.returnBundle(Bundle.class)).thenReturn(iQuery2);
-        Bundle bundle = mock(Bundle.class);
-        when(iQuery2.execute()).thenReturn(bundle);
-        Bundle.BundleEntryComponent bundleEntryComponent = mock(Bundle.BundleEntryComponent.class);
-        List<Bundle.BundleEntryComponent> bundleEntryComponents = new ArrayList<>();
-        bundleEntryComponents.add(bundleEntryComponent);
-
-        when(bundle.getEntry()).thenReturn(bundleEntryComponents);
-
-        PractitionerDto practitionerDto = mock(PractitionerDto.class);
-        List<PractitionerDto> practitionerDtos = new ArrayList<>();
-        practitionerDtos.add(practitionerDto);
-        when(modelMapper.map(bundleEntryComponent.getResource(), PractitionerDto.class)).thenReturn(practitionerDto);
-
-        //Act
-        List<PractitionerDto> practitionerDtoList = practitionerService.getAllPractitioners();
-
-        //Assert
-        assertEquals(practitionerDtos, practitionerDtoList);
-    }
-
-    @Test
-    public void testGetAllPractitioners_Given_NoPractitioner_Then_ThrowsException() {
-        //Arrange
-        thrown.expect(PractitionerNotFoundException.class);
-        thrown.expectMessage("No practitioners were found in the FHIR server");
-        IUntypedQuery iUntypedQuery = mock(IUntypedQuery.class);
-        when(fhirClient.search()).thenReturn(iUntypedQuery);
-        IQuery iQuery1 = mock(IQuery.class);
-        when(iUntypedQuery.forResource(Practitioner.class)).thenReturn(iQuery1);
-        IQuery iQuery2 = mock(IQuery.class);
-        when(iQuery1.returnBundle(Bundle.class)).thenReturn(iQuery2);
-        Bundle bundle = mock(Bundle.class);
-        when(iQuery2.execute()).thenReturn(bundle);
-        List<Bundle.BundleEntryComponent> bundleEntryComponents = new ArrayList<>();
-
-        when(bundle.getEntry()).thenReturn(bundleEntryComponents);
-
-        //Act
-        practitionerService.getAllPractitioners();
-
-        //Assert
-        //ExpectedException annotated by @rule is thrown;
-    }
+   }
 
 }
