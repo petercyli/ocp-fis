ddc1e4a84af13a62712f612660a2af4d17b3db3e
mingfan <ming.fan@feisystems.com>
2018-01-12 11:28:22 -0500

    Add pagination and searchType for organization
    #17169: As a user, I can see a list of organizations in the OCP, so that I know all of the organizations in the system.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/OrganizationService.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/OrganizationService.java
@@ -1,6 +1,7 @@
 package gov.samhsa.ocp.ocpfis.service;
 
 import gov.samhsa.ocp.ocpfis.service.dto.OrganizationDto;
+import gov.samhsa.ocp.ocpfis.web.OrganizationController;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,10 @@ public interface OrganizationService {
      *
      * @return
      */
-    List<OrganizationDto> getAllOrganizations(Optional<String> name);
+   // List<OrganizationDto> getAllOrganizations(Optional<String> name);
+
+    List<OrganizationDto> getAllOrganizations(Optional<Boolean> showInactive, Optional<Integer> page, Optional<Integer> size);
+
+    List<OrganizationDto> searchOrganizations(OrganizationController.SearchType searchType, String searchValue, Optional<Boolean> showInactive, Optional<Integer> page, Optional<Integer> size);
+
 }
--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/OrganizationController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/OrganizationController.java
@@ -2,12 +2,12 @@ package gov.samhsa.ocp.ocpfis.web;
 
 import gov.samhsa.ocp.ocpfis.service.OrganizationService;
 import gov.samhsa.ocp.ocpfis.service.dto.OrganizationDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,14 +16,25 @@ import java.util.Optional;
 @RequestMapping("/organizations")
 
 public class OrganizationController {
-    private final OrganizationService organizationService;
-
-    public OrganizationController(OrganizationService organizationService) {
-        this.organizationService = organizationService;
+    public enum SearchType {
+        identifier, name
     }
 
+    @Autowired
+    private OrganizationService organizationService;
+
+   // @GetMapping
+   // public List<OrganizationDto> getAllOrganization(@Valid @RequestParam(value = "name") Optional<String> name) {
+   //     return organizationService.getAllOrganizations(name);
+   // }
+
     @GetMapping
-    public List<OrganizationDto> getAllOrganization(@Valid @RequestParam(value = "name") Optional<String> name) {
-        return organizationService.getAllOrganizations(name);
+    public List<OrganizationDto> getAllOrganizations(@RequestParam Optional<Boolean> showInactive, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
+        return organizationService.getAllOrganizations(showInactive, page, size);
+    }
+
+    @GetMapping("/search")
+    public List<OrganizationDto> searchOrganizations(@RequestParam SearchType searchType, @RequestParam String searchValue, @RequestParam Optional<Boolean> showInactive, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
+        return organizationService.searchOrganizations(searchType, searchValue, showInactive, page, size);
     }
 }
