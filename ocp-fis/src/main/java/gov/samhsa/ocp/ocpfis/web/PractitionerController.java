397ddcb7419145de2b09c7df07aac501ca2ed710
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-17 15:31:16 -0500

    Add controller for Posting Practitioner
    #17174-As a user, I can enroll a new practitioner, so that practitioners can exist in the OCP

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PractitionerController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PractitionerController.java
@@ -5,10 +5,13 @@ import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
 import gov.samhsa.ocp.ocpfis.service.dto.PractitionerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -29,5 +32,10 @@ public class PractitionerController {
     @GetMapping("/search")
     public PageDto<PractitionerDto> searchPractitioners(@RequestParam SearchType searchType, @RequestParam String searchValue, @RequestParam Optional<Boolean> showInactive, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
         return practitionerService.searchPractitioners(searchType, searchValue, showInactive, page, size);
+    }
+
+    @PostMapping
+    public void createPractitioner(@Valid @RequestBody PractitionerDto practitionerDto){
+        practitionerService.createPractitioner(practitionerDto);
     }
 }
