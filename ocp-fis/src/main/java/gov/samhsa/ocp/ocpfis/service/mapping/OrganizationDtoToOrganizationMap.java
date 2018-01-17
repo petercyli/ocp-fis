851e194888284b98d9532a09f49b79def957000a
mingfan <ming.fan@feisystems.com>
2018-01-17 16:00:17 -0500

    Map Organization Dto to Fhir Organization
    #17157: As a user, I can create an organization, so that organizations can exist in the OCP

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/OrganizationDtoToOrganizationMap.java
@@ -0,0 +1,26 @@
+package gov.samhsa.ocp.ocpfis.service.mapping;
+
+import gov.samhsa.ocp.ocpfis.service.dto.OrganizationDto;
+import org.hl7.fhir.dstu3.model.Organization;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrganizationDtoToOrganizationMap extends PropertyMap<OrganizationDto, Organization> {
+    @Autowired
+    private IdentifierDtoListToIdentifierListConverter identifierDtoListToIdentifierListConverter;
+
+    @Autowired
+    private AddressDtoListToAddressListConverter addressDtoListToAddressListConverter;
+
+    @Autowired
+    private TelecomDtoListToTelecomListConverter telecomDtoListToTelecomListConverter;
+
+    @Override
+    protected void configure() {
+        map().setName(source.getName());
+        using(identifierDtoListToIdentifierListConverter).map(source.getIdentifiers()).setIdentifier(null);
+        using(addressDtoListToAddressListConverter).map(source.getAddresses()).setAddress(null);
+        using(telecomDtoListToTelecomListConverter).map(source.getTelecoms()).setTelecom(null);
+    }
+
+}
