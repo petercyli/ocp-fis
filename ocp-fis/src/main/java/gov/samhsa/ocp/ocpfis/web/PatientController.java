0292ec7715c8655007b73297021393a462a333d3
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-01-18 16:13:49 -0500

    Implement updatePatient method used to update a patient.
    17163 - As a user, I can enroll a new patient so that patients can exist in the OCP

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PatientController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/PatientController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,8 +51,15 @@ public class PatientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    void createPatient(@Valid @RequestBody PatientDto patientDto) {
+    public void createPatient(@Valid @RequestBody PatientDto patientDto) {
         patientService.createPatient(patientDto);
         log.info("Patient successfully created");
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePatient(@Valid @RequestBody PatientDto patientDto) {
+        patientService.updatePatient(patientDto);
+        log.info("Patient successfully updated");
     }
 }
