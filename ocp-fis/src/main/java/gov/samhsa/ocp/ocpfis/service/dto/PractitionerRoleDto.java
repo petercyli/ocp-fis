1cb642081c8002cc1208105ef48459384088960d
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-18 14:08:08 -0500

    Create PractitionerRoleDto
    #17174-As a user, I can enroll a new practitioner, so that practitioners can exist in the OCP.

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/PractitionerRoleDto.java
@@ -0,0 +1,16 @@
+package gov.samhsa.ocp.ocpfis.service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PractitionerRoleDto {
+    private String code;
+}
