1b0b88a667a659cfcf5ea86dc2e915f856d06dd8
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-01-31 13:59:42 -0500

    Add endpoints related to care-team-category, participant-type and care-team-status.
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/LookUpController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/LookUpController.java
@@ -179,4 +179,19 @@ public class LookUpController {
         return lookUpService.getLanguages();
     }
 
+    @GetMapping("/care-team-categories")
+    public List<ValueSetDto> getCareTeamCategories() {
+        return lookUpService.getCareTeamCategories();
+    }
+
+    @GetMapping("/participant-types")
+    public List<ValueSetDto> getParticipantTypes() {
+        return lookUpService.getParticipantTypes();
+    }
+
+    @GetMapping("/care-team-statuses")
+    public List<ValueSetDto> getCareTeamStatuses() {
+        return lookUpService.getCareTeamStatuses();
+    }
+
 }
