fca44b7c6461f9a13db22a53d1fe81b9b1b386fd
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-01 15:59:47 -0500

    Change list to pageDto for return value.
    #17183-As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/CareTeamController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/CareTeamController.java
@@ -1,14 +1,28 @@
 package gov.samhsa.ocp.ocpfis.web;
 
 import gov.samhsa.ocp.ocpfis.service.CareTeamService;
+import gov.samhsa.ocp.ocpfis.service.dto.CareTeamDto;
+import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/careTeams")
 public class CareTeamController {
 
     private final CareTeamService careTeamService;
 
     public CareTeamController(CareTeamService careTeamService) {
         this.careTeamService = careTeamService;
+    }
+
+    @GetMapping("/search")
+    private PageDto<CareTeamDto> getCareTeams(@RequestParam String searchType, @RequestParam String searchValue, @RequestParam Optional<String> showInactive, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size){
+        return careTeamService.getCareTeam(searchType,searchValue,showInactive,page,size);
     }
 }
