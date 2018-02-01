be77fe489a88d45315b2ad1824433730fa23416f
Shruti Rao <shruti.rao@feisystems.com>
2018-02-01 13:56:20 -0500

    Refactor healthCareService dto
    #17533:As a user, I can see a list of Health Care Services related to a location in the OCP, so that I know all of the Health Care Services in the system.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/HealthCareServiceDto.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/HealthCareServiceDto.java
@@ -3,23 +3,22 @@ package gov.samhsa.ocp.ocpfis.service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class HealthCareServiceDto {
-    private String logicalId;
+public class HealthCareServiceDto extends NameLogicalIdIdentifiersDto{
     private String organizationId;
     private boolean active;
-    private String name;
     private String categorySystem;
     private String categoryValue;
     private List<String> programName;
-    private List<IdentifierDto> identifier;
     private List<TelecomDto> telecom;
     private List<ValueSetDto> type;
     private List<NameLogicalIdIdentifiersDto> location;
