c1fa08117b4248f664052017c6bdb2a608819216
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-01 15:58:36 -0500

    Put pagination on careTeamService.
    #17183-As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/CareTeamService.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/CareTeamService.java
@@ -1,10 +1,16 @@
 package gov.samhsa.ocp.ocpfis.service;
 
 import gov.samhsa.ocp.ocpfis.service.dto.CareTeamDto;
+import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CareTeamService {
 
     void createCareTeam(CareTeamDto careTeamDto);
 
     void updateCareTeam(String careTeamId, CareTeamDto careTeamDto);
+
+    PageDto<CareTeamDto> getCareTeam(String searchType, String searchValue, Optional<String> showInactive,Optional<Integer> page, Optional<Integer> size);
 }
