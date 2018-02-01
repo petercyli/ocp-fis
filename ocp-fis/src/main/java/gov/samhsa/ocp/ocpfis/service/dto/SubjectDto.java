9eaf66d0992fc188e5efc7d6737820808586de8a
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-01 15:20:52 -0500

    Add lombok annotations
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/SubjectDto.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/SubjectDto.java
@@ -1,5 +1,14 @@
 package gov.samhsa.ocp.ocpfis.service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubjectDto {
     private String id;
 
