ce4bd7153c38d2d21403fe636316ad89cbde8d90
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-01 16:39:53 -0500

    Add a basic flow to save a CareTeam.
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/CareTeamServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/CareTeamServiceImpl.java
@@ -4,7 +4,11 @@ import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.validation.FhirValidator;
 import gov.samhsa.ocp.ocpfis.config.FisProperties;
 import gov.samhsa.ocp.ocpfis.service.dto.CareTeamDto;
+import gov.samhsa.ocp.ocpfis.service.exception.FHIRClientException;
+import gov.samhsa.ocp.ocpfis.service.mapping.dtotofhirmodel.CareTeamDtoToCareTeamConverter;
 import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.dstu3.model.CareTeam;
+import org.hl7.fhir.exceptions.FHIRException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,11 +36,31 @@ public class CareTeamServiceImpl implements CareTeamService {
 
     @Override
     public void createCareTeam(CareTeamDto careTeamDto) {
+        checkForDuplicates(careTeamDto);
 
+        try {
+            final CareTeam careTeam = CareTeamDtoToCareTeamConverter.map(careTeamDto);
+
+            validate(careTeam);
+
+            fhirClient.create().resource(careTeam).execute();
+
+        } catch (FHIRException e) {
+            throw new FHIRClientException("FHIR Client returned with an error while creating a care team:" + e.getMessage());
+
+        }
     }
 
     @Override
     public void updateCareTeam(String careTeamId, CareTeamDto careTeamDto) {
+
+    }
+
+    private void checkForDuplicates(CareTeamDto careTeamDto) {
+
+    }
+
+    private void validate(CareTeam careTeam) {
 
     }
 }
