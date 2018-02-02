d1a41a75871b43adc6c836f36cee4e29725d8fc7
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-02 15:26:07 -0500

    Use ParticipantSearchDto for participant search.
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/ParticipantService.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/ParticipantService.java
@@ -2,12 +2,12 @@ package gov.samhsa.ocp.ocpfis.service;
 
 import gov.samhsa.ocp.ocpfis.domain.ParticipantTypeEnum;
 import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
-import gov.samhsa.ocp.ocpfis.service.dto.ParticipantDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ParticipantSearchDto;
 
 import java.util.Optional;
 
 public interface ParticipantService {
 
-    PageDto<ParticipantDto> getAllParticipants(ParticipantTypeEnum participantType, String value, Optional<Boolean> showInActive, Optional<Integer> page, Optional<Integer> size);
+    PageDto<ParticipantSearchDto> getAllParticipants(ParticipantTypeEnum participantType, String value, Optional<Boolean> showInActive, Optional<Integer> page, Optional<Integer> size);
 
 }
