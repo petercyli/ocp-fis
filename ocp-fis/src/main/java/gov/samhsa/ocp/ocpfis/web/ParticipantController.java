2b4db99770c78dceec9b23ef9adefdd9b11a3282
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-02 15:28:21 -0500

    Use ParticipantSearchDto in the endpoint for participant search.
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ParticipantController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ParticipantController.java
@@ -4,7 +4,7 @@ package gov.samhsa.ocp.ocpfis.web;
 import gov.samhsa.ocp.ocpfis.domain.ParticipantTypeEnum;
 import gov.samhsa.ocp.ocpfis.service.ParticipantService;
 import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
-import gov.samhsa.ocp.ocpfis.service.dto.ParticipantDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ParticipantSearchDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,11 +23,11 @@ public class ParticipantController {
     }
 
     @GetMapping("/search")
-    public PageDto<ParticipantDto> getAllParticipants(@RequestParam(value = "member") ParticipantTypeEnum member,
-                                                      @RequestParam(value = "value") String value,
-                                                      @RequestParam(value = "showInActive", defaultValue = "false") Optional<Boolean> showInActive,
-                                                      @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                                      @RequestParam(value = "size", required = false) Optional<Integer> size) {
+    public PageDto<ParticipantSearchDto> getAllParticipants(@RequestParam(value = "member") ParticipantTypeEnum member,
+                                                            @RequestParam(value = "value") String value,
+                                                            @RequestParam(value = "showInActive", defaultValue = "false") Optional<Boolean> showInActive,
+                                                            @RequestParam(value = "page", required = false) Optional<Integer> page,
+                                                            @RequestParam(value = "size", required = false) Optional<Integer> size) {
         return participantService.getAllParticipants(member, value, showInActive, page, size);
     }
 
