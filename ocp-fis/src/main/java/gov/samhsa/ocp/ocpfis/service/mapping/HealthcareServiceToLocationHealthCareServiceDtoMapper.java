d48bfdc1449292470a2f90e5a5699751f2e1442f
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-07 15:23:59 -0500

    Add health care service to location health care service dto mapper.
    #17183-As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/HealthcareServiceToLocationHealthCareServiceDtoMapper.java
@@ -0,0 +1,44 @@
+package gov.samhsa.ocp.ocpfis.service.mapping;
+
+import gov.samhsa.ocp.ocpfis.service.dto.HealthCareServiceDto;
+import gov.samhsa.ocp.ocpfis.service.dto.LocationHealthCareServiceDto;
+import org.hl7.fhir.dstu3.model.HealthcareService;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthcareServiceToLocationHealthCareServiceDtoMapper extends PropertyMap<HealthcareService, LocationHealthCareServiceDto> {
+
+    private final TelecomListToTelecomDtoListConverter telecomListToTelecomDtoListConverter;
+
+    private final IdentifierListToIdentifierDtoListConverter identifierListToIdentifierDtoListConverter;
+
+    private final HealthecareServiceTypeListToHealthcareServiceTypeDtoListConverter healthecareServiceTypeListToHealthcareServiceTypeDtoListConverter;
+
+    private final HealthecareServiceCategoryToHealthcareServiceCategoryDtoConverter healthecareServiceCategoryToHealthcareServiceCategoryDtoConverter;
+
+
+    @Autowired
+    public HealthcareServiceToLocationHealthCareServiceDtoMapper(TelecomListToTelecomDtoListConverter telecomListToTelecomDtoListConverter, IdentifierListToIdentifierDtoListConverter identifierListToIdentifierDtoListConverter, HealthecareServiceTypeListToHealthcareServiceTypeDtoListConverter healthecareServiceTypeListToHealthcareServiceTypeDtoListConverter, HealthecareServiceCategoryToHealthcareServiceCategoryDtoConverter healthecareServiceCategoryToHealthcareServiceCategoryDtoConverter) {
+        this.telecomListToTelecomDtoListConverter = telecomListToTelecomDtoListConverter;
+        this.identifierListToIdentifierDtoListConverter = identifierListToIdentifierDtoListConverter;
+        this.healthecareServiceTypeListToHealthcareServiceTypeDtoListConverter = healthecareServiceTypeListToHealthcareServiceTypeDtoListConverter;
+        this.healthecareServiceCategoryToHealthcareServiceCategoryDtoConverter = healthecareServiceCategoryToHealthcareServiceCategoryDtoConverter;
+    }
+
+    @Override
+    protected void configure() {
+        map().setResourceURL(source.getId());
+        map().setName(source.getName());
+        map().setActive(source.getActive());
+        map().setOrganizationId(source.getProvidedBy().getReference());
+        map().setOrganizationName(source.getProvidedBy().getDisplay());
+        map().setCategorySystem(source.getCategory().getCodingFirstRep().getSystem());
+        map().setCategoryValue(source.getCategory().getCodingFirstRep().getCode());
+        using(telecomListToTelecomDtoListConverter).map(source.getTelecom()).setTelecom(null);
+        using(identifierListToIdentifierDtoListConverter).map(source.getIdentifier()).setIdentifiers(null);
+        using(healthecareServiceTypeListToHealthcareServiceTypeDtoListConverter).map(source.getType()).setType(null);
+        using(healthecareServiceCategoryToHealthcareServiceCategoryDtoConverter).map(source.getCategory()).setCategory(null);
+    }
+}
