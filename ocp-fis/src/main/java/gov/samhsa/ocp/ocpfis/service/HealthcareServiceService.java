b0d1b47237ac6341028d548424cd94f76315e3be
Shruti Rao <shruti.rao@feisystems.com>
2018-02-14 15:45:57 -0500

    Add API to inactivate a healthcare service
    #17162: As a user, I can mark a Healthcare Service as Inactive, so that the Healthcare Service can be removed from OCP

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/HealthcareServiceService.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/HealthcareServiceService.java
@@ -8,13 +8,20 @@ import java.util.Optional;
 
 public interface HealthcareServiceService {
     PageDto<HealthcareServiceDto> getAllHealthcareServices(Optional<List<String>> statusList, Optional<String> searchKey, Optional<String> searchValue, Optional<Integer> page, Optional<Integer> size);
+
     PageDto<HealthcareServiceDto> getAllHealthcareServicesByOrganization(String organizationResourceId, Optional<String> assignedToLocationId, Optional<List<String>> statusList, Optional<String> searchKey, Optional<String> searchValue, Optional<Integer> page, Optional<Integer> size);
+
     PageDto<HealthcareServiceDto> getAllHealthcareServicesByLocation(String organizationResourceId, String locationId, Optional<List<String>> statusList, Optional<String> searchKey, Optional<String> searchValue, Optional<Integer> page, Optional<Integer> size);
+
     HealthcareServiceDto getHealthcareService(String healthcareServiceId);
 
     void createHealthcareService(String organizationId, HealthcareServiceDto healthcareServiceDto);
+
     void updateHealthcareService(String organizationId, String healthcareServiceId, HealthcareServiceDto healthcareServiceDto);
 
+    void inactivateHealthcareService(String healthcareServiceId);
+
     void assignLocationToHealthcareService(String healthcareServiceId, String organizationResourceId, List<String> locationIdList);
+
     void unassignLocationToHealthcareService(String healthcareServiceId, String organizationResourceId, List<String> locationIdList);
 }
