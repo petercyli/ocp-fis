8981378b8ea1ace7e84ef1b3855e877589efa3d4
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-16 14:26:24 -0500

    Put organization in front of controller
    #17899:As a user, I can create activity definition at organization level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ActivityDefinitionController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ActivityDefinitionController.java
@@ -4,6 +4,7 @@ import gov.samhsa.ocp.ocpfis.service.ActivityDefinitionService;
 import gov.samhsa.ocp.ocpfis.service.dto.ActivityDefinitionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class ActivityDefinitionController {
     @Autowired
     private ActivityDefinitionService activityDefinitionService;
 
-    @RequestMapping("{organizationId}/activity-definitions")
+    @PostMapping("organization/{organizationId}/activity-definitions")
     public void createActivityDefinition(@RequestBody ActivityDefinitionDto activityDefinitionDto,@PathVariable String organizationId){
        activityDefinitionService.createActivityDefinition(activityDefinitionDto,organizationId);
     }
