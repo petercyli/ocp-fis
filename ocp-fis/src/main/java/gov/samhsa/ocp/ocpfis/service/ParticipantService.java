b3593c1e55f09b97d60035a26c3c449554163a90
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-19 14:29:57 -0500

    Add patientId when searching for participants of type relatedPersons.
    17550 - As a user, I would like to add related person to patient

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/ParticipantService.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/ParticipantService.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface ParticipantService {
 
-    PageDto<ParticipantSearchDto> getAllParticipants(ParticipantTypeEnum participantType, String value, Optional<Boolean> showInActive, Optional<Integer> page, Optional<Integer> size);
+    PageDto<ParticipantSearchDto> getAllParticipants(String patientId, ParticipantTypeEnum participantType, String value, Optional<Boolean> showInActive, Optional<Integer> page, Optional<Integer> size);
 
 }
