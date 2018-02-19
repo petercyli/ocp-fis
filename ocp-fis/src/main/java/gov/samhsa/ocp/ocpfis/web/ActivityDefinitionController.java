66e5e6ee654cd32470d9c4e8363dc1e1875511c4
mingfan <ming.fan@feisystems.com>
2018-02-19 09:32:11 -0500

    Implement the controller to list activity definition
    #17899: As a user, I can create activity definition at organization level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ActivityDefinitionController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ActivityDefinitionController.java
@@ -2,16 +2,33 @@ package gov.samhsa.ocp.ocpfis.web;
 
 import gov.samhsa.ocp.ocpfis.service.ActivityDefinitionService;
 import gov.samhsa.ocp.ocpfis.service.dto.ActivityDefinitionDto;
+import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class ActivityDefinitionController {
 
     @Autowired
     private ActivityDefinitionService activityDefinitionService;
+
+
+    @GetMapping("/organizations/{organizationId}/activity-definitions")
+    public PageDto<ActivityDefinitionDto> getAllHealthcareServicesByOrganization(@PathVariable String organizationId,
+                                                                                 @RequestParam(value = "searchKey") Optional<String> searchKey,
+                                                                                 @RequestParam(value = "searchValue") Optional<String> searchValue,
+                                                                                 @RequestParam(value = "pageNumber") Optional<Integer> pageNumber,
+                                                                                 @RequestParam(value = "pageSize") Optional<Integer> pageSize) {
+        return activityDefinitionService.getAllActivityDefinitionsByOrganization(organizationId, searchKey, searchValue, pageNumber, pageSize);
+    }
+
 
     @RequestMapping("/activity-definitions")
     public void createActivityDefinition(@RequestBody ActivityDefinitionDto activityDefinitionDto){
