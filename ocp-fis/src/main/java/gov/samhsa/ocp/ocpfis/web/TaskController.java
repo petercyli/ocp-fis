e0eb9bdc25c9ef37018e096c9b6b7aa5c5e26bd1
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-21 14:59:27 -0500

    Make controller to update the task.
    #17900:As a user I will create task for a patient based on activity definition

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/TaskController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/TaskController.java
@@ -4,7 +4,9 @@ import gov.samhsa.ocp.ocpfis.service.TaskService;
 import gov.samhsa.ocp.ocpfis.service.dto.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class TaskController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createTask(@Valid @RequestBody TaskDto taskDto) {
         taskService.createTask(taskDto);
+    }
+
+    @PutMapping("/tasks/{taskId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateTask(@PathVariable String taskId, @Valid @RequestBody TaskDto taskDto){
+        taskService.updateTask(taskId,taskDto);
     }
 }
