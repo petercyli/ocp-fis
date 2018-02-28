8ac01b37f91c4316fd53eed74c5a365e21681f52
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-28 16:35:49 -0500

    Add an endpoint to retrieve participants based on patient and roles.
    18199 - As a user, I will be able to edit/update the tasks at patient level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ParticipantController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ParticipantController.java
@@ -2,14 +2,17 @@ package gov.samhsa.ocp.ocpfis.web;
 
 
 import gov.samhsa.ocp.ocpfis.domain.ParticipantTypeEnum;
+import gov.samhsa.ocp.ocpfis.service.CareTeamService;
 import gov.samhsa.ocp.ocpfis.service.ParticipantService;
 import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ParticipantDto;
 import gov.samhsa.ocp.ocpfis.service.dto.ParticipantSearchDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,9 +20,11 @@ import java.util.Optional;
 public class ParticipantController {
 
     private final ParticipantService participantService;
+    private final CareTeamService careTeamService;
 
-    public ParticipantController(ParticipantService participantService) {
+    public ParticipantController(ParticipantService participantService, CareTeamService careTeamService) {
         this.participantService = participantService;
+        this.careTeamService = careTeamService;
     }
 
     @GetMapping("/search")
@@ -30,6 +35,12 @@ public class ParticipantController {
                                                             @RequestParam(value = "page", required = false) Optional<Integer> page,
                                                             @RequestParam(value = "size", required = false) Optional<Integer> size) {
         return participantService.getAllParticipants(patientId, member, value, showInActive, page, size);
+    }
+
+    @GetMapping
+    public List<ParticipantDto> getCareTeamParticipants(@RequestParam(value = "patient") String patient,
+                                                      @RequestParam(value = "roles") List<String> roles) {
+        return careTeamService.getCareTeamParticipants(patient, roles);
     }
 
 }
