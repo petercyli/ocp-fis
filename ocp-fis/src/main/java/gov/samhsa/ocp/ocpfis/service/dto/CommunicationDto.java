053498d9efa5302c460e725aa64133fbb3df056f
mingfan <ming.fan@feisystems.com>
2018-02-28 14:51:21 -0500

    Define CommunicationDto
    #18345: As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/CommunicationDto.java
@@ -0,0 +1,60 @@
+package gov.samhsa.ocp.ocpfis.service.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CommunicationDto {
+    private String logicalId;
+
+    //Comments about Communication.
+    private String note;
+
+    private boolean notDone;
+
+    private ValueSetDto status;
+    private ValueSetDto notDoneReason;
+    private ValueSetDto category;
+    private ValueSetDto medium;
+
+
+    /*To be supported later once organization relationship is established.
+    Allow user to select from ActivityDefinition list for the organization
+    in the current context and kind = Communication. There could be one or
+    more ActivityDefinition templates defined for Communication (e.g. Outreach).
+     */
+    private ReferenceDto definition;
+
+    private ReferenceDto topic;
+    private ReferenceDto subject;
+    private List<ReferenceDto> recipient;
+    private ReferenceDto sender;
+
+    /* Select from list of Episode of Care for current patient context
+     * and current organization context. Generally there should be only
+     * one active Episode of Care for given patient and organization.
+     * Keeping it optional to allow communication that is not related to
+     * specific episode of care.*/
+    private ContextDto context;
+
+    //Communication Sent Date
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate sent;
+
+    //Communication Received Date
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate received;
+
+
+}
