37eb5f516a2e179aad23bc0eac0212ad0ada000e
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-03-05 15:11:58 -0500

    Add a method to create ReferenceDto from Task and associated EpisodeOfCares.
    18199 - As a user, I will be able to edit/update the tasks at patient level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/EpisodeOfCareToEpisodeOfCareDtoMapper.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/EpisodeOfCareToEpisodeOfCareDtoMapper.java
@@ -1,17 +1,27 @@
 package gov.samhsa.ocp.ocpfis.service.mapping;
 
 import gov.samhsa.ocp.ocpfis.service.dto.EpisodeOfCareDto;
-//import gov.samhsa.ocp.ocpfis.util.FhirUtils;
+import gov.samhsa.ocp.ocpfis.service.dto.ReferenceDto;
 import gov.samhsa.ocp.ocpfis.util.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.EpisodeOfCare;
 import org.hl7.fhir.dstu3.model.Period;
+import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.fhir.dstu3.model.ResourceType;
+import org.hl7.fhir.dstu3.model.Task;
+import org.hl7.fhir.exceptions.FHIRException;
 
 import java.util.List;
+import java.util.StringJoiner;
 
+//import gov.samhsa.ocp.ocpfis.util.FhirUtils;
+
+@Slf4j
 public class EpisodeOfCareToEpisodeOfCareDtoMapper {
+
+    public static final String NA = "NA";
 
     public static EpisodeOfCareDto map(EpisodeOfCare episodeOfCare) {
         EpisodeOfCareDto dto = new EpisodeOfCareDto();
@@ -60,5 +70,41 @@ public class EpisodeOfCareToEpisodeOfCareDtoMapper {
         }
 
         return dto;
+    }
+
+    public static ReferenceDto mapToReferenceDto(Task task) {
+        ReferenceDto dto = new ReferenceDto();
+
+        dto.setReference(task.getContext().getReference());
+
+        if (task.hasContext()) {
+            EpisodeOfCare episodeOfCare = (EpisodeOfCare) task.getContext().getResource();
+            dto.setDisplay(createDisplayForEpisodeOfCare(task, episodeOfCare));
+        }
+
+        return dto;
+    }
+
+    private static String createDisplayForEpisodeOfCare(Task task, EpisodeOfCare episodeOfCare) {
+        ReferenceDto activityDefinitionRefDto = null;
+        try {
+            activityDefinitionRefDto = convertReferenceToReferenceDto(task.getDefinitionReference());
+        } catch (FHIRException e) {
+            log.error("ReferenceDto for ActivityDefintion could not be retrieved");
+        }
+
+        String eocType = activityDefinitionRefDto != null ? activityDefinitionRefDto.getDisplay() : NA;
+        String date = episodeOfCare.getPeriod() != null ? DateUtil.convertToString(episodeOfCare.getPeriod().getStart()) : NA;
+        String agent = episodeOfCare.getCareManager() != null ? episodeOfCare.getCareManager().getDisplay() : NA;
+
+
+        return new StringJoiner("-").add(eocType).add(date).add(agent).toString();
+    }
+
+    private static ReferenceDto convertReferenceToReferenceDto(Reference reference) {
+        ReferenceDto referenceDto = new ReferenceDto();
+        referenceDto.setDisplay(reference.getDisplay());
+        referenceDto.setReference(reference.getReference());
+        return referenceDto;
     }
 }
