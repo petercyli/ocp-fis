46e122512fd2657b7af07d6662b3b9a4c9d8f0c1
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-03-05 15:13:27 -0500

    Add API to retrieve episodeOfCares as list of ReferenceDtos.
    18199 - As a user, I will be able to edit/update the tasks at patient level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/EpisodeOfCareServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/EpisodeOfCareServiceImpl.java
@@ -7,10 +7,13 @@ import ca.uhn.fhir.rest.gclient.TokenClientParam;
 import ca.uhn.fhir.validation.FhirValidator;
 import gov.samhsa.ocp.ocpfis.config.FisProperties;
 import gov.samhsa.ocp.ocpfis.service.dto.EpisodeOfCareDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ReferenceDto;
 import gov.samhsa.ocp.ocpfis.service.mapping.EpisodeOfCareToEpisodeOfCareDtoMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.EpisodeOfCare;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.hl7.fhir.dstu3.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -61,5 +64,31 @@ public class EpisodeOfCareServiceImpl implements EpisodeOfCareService {
         }
 
         return episodeOfCareDtos;
+    }
+
+    @Override
+    public List<ReferenceDto> getEpisodeOfCaresForReference(String patient, Optional<String> status) {
+        List<ReferenceDto> referenceDtos = new ArrayList<>();
+
+        IQuery iQuery = fhirClient.search().forResource(Task.class)
+                .where(new ReferenceClientParam("patient").hasId(ResourceType.Patient + "/" + patient))
+                .include(Task.INCLUDE_CONTEXT);
+
+        Bundle bundle = (Bundle) iQuery.returnBundle(Bundle.class).execute();
+
+        if (bundle != null) {
+            List<Bundle.BundleEntryComponent> eocCompoents = bundle.getEntry();
+
+            if (eocCompoents != null) {
+                referenceDtos = eocCompoents.stream()
+                        .filter(it -> it.getResource().getResourceType().equals(ResourceType.Task))
+                        .map(it -> (Task) it.getResource())
+                        .map(it -> EpisodeOfCareToEpisodeOfCareDtoMapper.mapToReferenceDto(it))
+                        .collect(toList());
+            }
+        }
+
+        return referenceDtos;
+
     }
 }
