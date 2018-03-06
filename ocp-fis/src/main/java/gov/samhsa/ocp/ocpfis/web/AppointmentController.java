ad1714c5e72d9eb7a0d11a36d3fbbfa0b79b4fc0
mingfan <ming.fan@feisystems.com>
2018-03-06 12:57:39 -0500

    Implement the controller to list Appointment for a patient
    #18176: As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/AppointmentController.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/AppointmentController.java
@@ -2,14 +2,20 @@ package gov.samhsa.ocp.ocpfis.web;
 
 import gov.samhsa.ocp.ocpfis.service.AppointmentService;
 import gov.samhsa.ocp.ocpfis.service.dto.AppointmentDto;
+import gov.samhsa.ocp.ocpfis.service.dto.CommunicationDto;
+import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/appointments")
@@ -26,4 +32,14 @@ public class AppointmentController {
     public void createAppointment(@Valid @RequestBody AppointmentDto appointmentDto) {
         appointmentService.createAppointment(appointmentDto);
     }
+
+    @GetMapping("/search")
+    public PageDto<AppointmentDto> getAppointments(@RequestParam Optional<List<String>> statusList,
+                                                   @RequestParam(value = "searchKey") String searchKey,
+                                                   @RequestParam(value = "searchValue") String searchValue,
+                                                   @RequestParam(value = "pageNumber") Optional<Integer> pageNumber,
+                                                   @RequestParam(value = "pageSize") Optional<Integer> pageSize) {
+        return appointmentService.getAppointments(statusList, searchKey, searchValue, pageNumber, pageSize);
+    }
+
 }
