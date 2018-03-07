96dfb6cf29ec8747ba1eec950c893b1c81d5b296
CindyLiFei <cindy.li@feisystems.com>
2018-03-07 15:55:42 -0500

    Fix import error
    #18132 : As a user I should be able to associate a practitioner with one or more organizations with practitioner role.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
