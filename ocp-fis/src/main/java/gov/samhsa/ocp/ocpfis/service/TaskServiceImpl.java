d8e55bd96fb0856da3b71573d101e00797450d92
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-03-07 11:08:36 -0500

    Refactor to use TaskDtoToTaskMap.
    18199 - As a user, I will be able to edit/update the tasks at patient level

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/TaskServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/TaskServiceImpl.java
@@ -16,6 +16,7 @@ import gov.samhsa.ocp.ocpfis.service.dto.ValueSetDto;
 import gov.samhsa.ocp.ocpfis.service.exception.DuplicateResourceFoundException;
 import gov.samhsa.ocp.ocpfis.service.exception.ResourceNotFoundException;
 import gov.samhsa.ocp.ocpfis.service.mapping.TaskToTaskDtoMap;
+import gov.samhsa.ocp.ocpfis.service.mapping.dtotofhirmodel.TaskDtoToTaskMap;
 import gov.samhsa.ocp.ocpfis.util.DateUtil;
 import gov.samhsa.ocp.ocpfis.util.FhirDtoUtil;
 import gov.samhsa.ocp.ocpfis.util.PaginationUtil;
@@ -136,7 +137,7 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void createTask(TaskDto taskDto) {
         if (!isDuplicate(taskDto)) {
-            Task task = setTaskDtoToTask(taskDto);
+            Task task = TaskDtoToTaskMap.map(taskDto);
 
             //Checking activity definition for enrollment
             task.setContext(createOrRetrieveEpisodeOfCare(taskDto));
@@ -152,7 +153,7 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void updateTask(String taskId, TaskDto taskDto) {
-        Task task = setTaskDtoToTask(taskDto);
+        Task task = TaskDtoToTaskMap.map(taskDto);
         task.setId(taskId);
 
         Bundle taskBundle = fhirClient.search().forResource(Task.class)
@@ -315,72 +316,6 @@ public class TaskServiceImpl implements TaskService {
             }).collect(Collectors.toList());
         }
         return !duplicateCheckList.isEmpty();
-    }
-
-    private Task setTaskDtoToTask(TaskDto taskDto) {
-        Task task = new Task();
-        task.setDefinition(FhirDtoUtil.mapReferenceDtoToReference(taskDto.getDefinition()));
-
-        if (taskDto.getPartOf() != null) {
-            List<Reference> partOfReferences = new ArrayList<>();
-            partOfReferences.add(mapReferenceDtoToReference(taskDto.getPartOf()));
-            task.setPartOf(partOfReferences);
-        }
-
-        task.setStatus(Task.TaskStatus.valueOf(taskDto.getStatus().getCode().toUpperCase()));
-        task.setIntent(Task.TaskIntent.valueOf(taskDto.getIntent().getCode().toUpperCase()));
-        task.setPriority(Task.TaskPriority.valueOf(taskDto.getPriority().getCode().toUpperCase()));
-
-        if (taskDto.getDescription() != null && !taskDto.getDescription().isEmpty()) {
-            task.setDescription(taskDto.getDescription());
-        }
-
-        task.setFor(mapReferenceDtoToReference(taskDto.getBeneficiary()));
-
-        //Set execution Period
-        if (taskDto.getExecutionPeriod() != null) {
-            if (taskDto.getExecutionPeriod().getStart() != null)
-                task.getExecutionPeriod().setStart(java.sql.Date.valueOf(taskDto.getExecutionPeriod().getStart()));
-        } else if (taskDto.getStatus().getCode().equalsIgnoreCase(TaskStatus.INPROGRESS.toCode()))
-            task.getExecutionPeriod().setStart(java.sql.Date.valueOf(LocalDate.now()));
-
-        if (taskDto.getExecutionPeriod() != null) {
-            if (taskDto.getExecutionPeriod().getEnd() != null)
-                task.getExecutionPeriod().setEnd(java.sql.Date.valueOf(taskDto.getExecutionPeriod().getEnd()));
-        } else if (taskDto.getStatus().getCode().equalsIgnoreCase(TaskStatus.COMPLETED.toCode()))
-            task.getExecutionPeriod().setEnd(java.sql.Date.valueOf(LocalDate.now()));
-
-        //Set agent
-        task.getRequester().setAgent(mapReferenceDtoToReference(taskDto.getAgent()));
-
-        //Set on Behalf of
-        if (taskDto.getOnBehalfOf() != null) {
-            task.getRequester().setOnBehalfOf(mapReferenceDtoToReference(taskDto.getOnBehalfOf()));
-        }
-
-        //Set PerformerType
-        if (taskDto.getPerformerType() != null) {
-            List<CodeableConcept> codeableConcepts = new ArrayList<>();
-            CodeableConcept codeableConcept = new CodeableConcept();
-            codeableConcept.addCoding().setCode(taskDto.getPerformerType().getCode())
-                    .setDisplay(taskDto.getPerformerType().getDisplay())
-                    .setSystem(taskDto.getPerformerType().getSystem());
-            codeableConcepts.add(codeableConcept);
-            task.setPerformerType(codeableConcepts);
-        }
-
-        //Set last Modified
-        task.setLastModified(java.sql.Date.valueOf(LocalDate.now()));
-
-        task.setOwner(mapReferenceDtoToReference(taskDto.getOwner()));
-
-        Annotation annotation = new Annotation();
-        annotation.setText(taskDto.getNote());
-        List<Annotation> annotations = new ArrayList<>();
-        annotations.add(annotation);
-        task.setNote(annotations);
-
-        return task;
     }
 
     private EpisodeOfCare createEpisodeOfCare(TaskDto taskDto) {
