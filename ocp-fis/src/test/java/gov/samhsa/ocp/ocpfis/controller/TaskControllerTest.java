a7837464a7c32eabd24e26c2e6d9d03a8be432ce
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-03-12 13:25:09 -0400

    Write unit test to getRelatedTask task.

--- ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/controller/TaskControllerTest.java
+++ ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/controller/TaskControllerTest.java
@@ -2,6 +2,7 @@ package gov.samhsa.ocp.ocpfis.controller;
 
 import gov.samhsa.ocp.ocpfis.service.LookUpService;
 import gov.samhsa.ocp.ocpfis.service.TaskService;
+import gov.samhsa.ocp.ocpfis.service.dto.ReferenceDto;
 import gov.samhsa.ocp.ocpfis.service.dto.TaskDto;
 import gov.samhsa.ocp.ocpfis.web.TaskController;
 import org.hamcrest.CoreMatchers;
@@ -24,9 +25,8 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = TaskController.class, secure = false)
-@Ignore("Depends on config server of bootstrap")
+@Ignore
 public class TaskControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
@@ -38,16 +38,34 @@ public class TaskControllerTest {
 
     private final String TASK_JSON = "[{\"logicalId\":\"1\",\"definition\":null,\"partOf\":null,\"status\":null,\"intent\":null,\"priority\":null,\"description\":\"This is description\",\"beneficiary\":null,\"context\":null,\"executionPeriod\":null,\"authoredOn\":null,\"lastModified\":null,\"agent\":null,\"performerType\":null,\"owner\":null,\"note\":\"note\",\"organization\":null}]";
 
+    private final String REFERENCE_TASK_JSON = "[{\"reference\":\"1\",\"display\":\"display\"}]";
+
+    @Test
+    public void testGetRelatedTasks() throws Exception {
+        //Arrange
+        ReferenceDto referenceDto = createReference();
+        List<ReferenceDto> referenceDtoList = new ArrayList<>();
+        referenceDtoList.add(referenceDto);
+        Mockito.when(taskService.getRelatedTasks("12")).thenReturn(referenceDtoList);
+
+        //Act
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/tasks/task-references?patient=12");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        //Assert
+        Assert.assertThat(result.getResponse().getContentAsString(), CoreMatchers.containsString(REFERENCE_TASK_JSON));
+    }
+
     @Test
     public void testGetUpcomingTasks() throws Exception {
         //Arrange
         TaskDto taskDto = createTask();
         List<TaskDto> taskDtoList = new ArrayList<>();
         taskDtoList.add(taskDto);
-        Mockito.when(taskService.getUpcomingTasks("practitioner")).thenReturn(taskDtoList);
+        Mockito.when(taskService.getUpcomingTasks("234")).thenReturn(taskDtoList);
 
         //Act
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/tasks/upcoming?practitioner=practitioner");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/tasks?practitioner=234");
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
         //Assert
@@ -60,5 +78,12 @@ public class TaskControllerTest {
         taskDto.setDescription("This is description");
         taskDto.setNote("note");
         return taskDto;
+    }
+
+    private ReferenceDto createReference() {
+        ReferenceDto referenceDto = new ReferenceDto();
+        referenceDto.setReference("1");
+        referenceDto.setDisplay("display");
+        return referenceDto;
     }
 }
