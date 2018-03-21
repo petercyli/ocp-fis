4656156403f32de1c742f187b7b36bd2bbf93198
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-03-21 16:41:25 -0400

    Fix the duplicate flag bug

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PatientServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PatientServiceImpl.java
@@ -427,7 +427,7 @@ public class PatientServiceImpl implements PatientService {
                 List<Flag> flags = duplicateCheckList.stream()
                         .filter(flag -> flagDto.getLogicalId().equalsIgnoreCase(flag.getIdElement().getIdPart())
                         ).collect(toList());
-                return !flags.isEmpty();
+                return flags.isEmpty();
             }
         } else {
             //Checking while creating new flag
