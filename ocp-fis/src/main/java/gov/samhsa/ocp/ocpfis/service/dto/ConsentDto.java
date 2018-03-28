802b4b68c67319210a05bedbcf35bcf0e23cd4bf
mingfan <ming.fan@feisystems.com>
2018-03-28 16:28:38 -0400

    Create ConsentDto
    #18851: As a Patient, I can view the draft consent created by the care coordinator and attest the consent so that care teams can have access to my health records

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/ConsentDto.java
@@ -0,0 +1,45 @@
+package gov.samhsa.ocp.ocpfis.service.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConsentDto {
+
+    private String logicalId;
+
+    private IdentifierDto identifier;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate endDate;
+
+    private LocalDateTime dateTime;
+
+    private String status;
+
+    private boolean isGeneralDesignation;
+
+    private ReferenceDto patient;
+
+    private List<ReferenceDto> fromActor;
+
+    private List<ReferenceDto> toActor;
+
+    private String fromGeneralDesignation;
+
+    private String toGeneralDesignation;
+}
