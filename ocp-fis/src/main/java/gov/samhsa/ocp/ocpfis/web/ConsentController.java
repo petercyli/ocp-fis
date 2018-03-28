3113605fb1607b4dbaadf9c74a7d5fb79ffb4c7e
mingfan <ming.fan@feisystems.com>
2018-03-28 16:27:41 -0400

    Initialize controller
    #18851: As a Patient, I can view the draft consent created by the care coordinator and attest the consent so that care teams can have access to my health records

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/web/ConsentController.java
@@ -0,0 +1,26 @@
+package gov.samhsa.ocp.ocpfis.web;
+
+import gov.samhsa.ocp.ocpfis.service.ConsentService;
+import gov.samhsa.ocp.ocpfis.service.dto.ConsentDto;
+import gov.samhsa.ocp.ocpfis.service.dto.PageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+public class ConsentController {
+    @Autowired
+    private ConsentService consentService;
+
+    @GetMapping("/consents")
+    public PageDto<ConsentDto> getConsents(@RequestParam(value = "patient") Optional<String> patient,
+                                           @RequestParam(value = "fromActor") Optional<String> fromActor,
+                                           @RequestParam(value = "toActor") Optional<String> toActor,
+                                           @RequestParam(value = "isGeneralDesignation") Optional<Boolean> isGeneralDesignation,
+                                           @RequestParam(value = "status") Optional<String> status,
+                                           @RequestParam Optional<Integer> pageNumber,
+                                           @RequestParam Optional<Integer> pageSize) {
+        return consentService.getConsents(patient, fromActor, toActor,isGeneralDesignation, status, pageNumber, pageSize);
+    }
+}
