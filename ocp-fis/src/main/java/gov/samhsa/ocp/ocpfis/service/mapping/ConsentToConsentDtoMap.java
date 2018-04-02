f3b2845b32d62bab4c530883472327c10bde9fb8
mingfan <ming.fan@feisystems.com>
2018-04-02 17:31:34 -0400

    Map identifier of consent
    #18851: As a Patient, I can view the draft consent created by the care coordinator and attest the consent so that care teams can have access to my health records

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/ConsentToConsentDtoMap.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/ConsentToConsentDtoMap.java
@@ -13,16 +13,19 @@ public class ConsentToConsentDtoMap extends PropertyMap<Consent, ConsentDto> {
     private final ResourceIdToLogicalIdConverter resourceIdToLogicalIdConverter;
     private final PeriodToPeriodDtoConverter periodToPeriodDtoConverter;
     private final CodeableConceptListToValueSetDtoListConverter codeableConceptListToValueSetDtoListConverter;
+    private final IdentifierToIdentifierDtoConverter identifierToIdentifierDtoConverter;
 
     @Autowired
     public ConsentToConsentDtoMap(ConsentActorComponentListToReferenceDtoListConverter consentActorComponentListToReferenceDtoListConverter,
                                   ResourceIdToLogicalIdConverter resourceIdToLogicalIdConverter,
                                   PeriodToPeriodDtoConverter periodToPeriodDtoConverter,
-                                  CodeableConceptListToValueSetDtoListConverter CodeableConceptListToValueSetDtoListConverter) {
+                                  CodeableConceptListToValueSetDtoListConverter CodeableConceptListToValueSetDtoListConverter,
+                                  IdentifierToIdentifierDtoConverter identifierToIdentifierDtoConverter) {
         this.consentActorComponentListToReferenceDtoListConverter = consentActorComponentListToReferenceDtoListConverter;
         this.resourceIdToLogicalIdConverter = resourceIdToLogicalIdConverter;
         this.periodToPeriodDtoConverter = periodToPeriodDtoConverter;
         this.codeableConceptListToValueSetDtoListConverter = CodeableConceptListToValueSetDtoListConverter;
+        this.identifierToIdentifierDtoConverter = identifierToIdentifierDtoConverter;
     }
 
     @Override
@@ -32,6 +35,7 @@ public class ConsentToConsentDtoMap extends PropertyMap<Consent, ConsentDto> {
         using(consentActorComponentListToReferenceDtoListConverter).map(source.getActor()).setToActor(null);
         using(periodToPeriodDtoConverter).map(source.getPeriod()).setPeriod(null);
         using(codeableConceptListToValueSetDtoListConverter).map(source.getCategory()).setCategory(null);
+        using(identifierToIdentifierDtoConverter).map(source.getIdentifier()).setIdentifier(null);
     }
 
 }
