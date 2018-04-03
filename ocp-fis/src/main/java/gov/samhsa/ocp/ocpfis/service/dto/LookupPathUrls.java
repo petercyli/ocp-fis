cc5c213daaee1a42dd5f7eb343673b0bf32be812
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-04-03 14:39:03 -0400

    Change the LookupPathUrls for consent lookups.
    #18779:As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record.

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/LookupPathUrls.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/dto/LookupPathUrls.java
@@ -113,10 +113,10 @@ public enum LookupPathUrls {
         static final String PROVIDER_SPECIALTY_PATH = "/ValueSet/ocp-provider-specialty";
         static final String FLAG_STATUS_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/flag-status";
         static final String FLAG_CATEGORY_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/flag-category";
-        static final String CONSENT_STATE_PATH="/ValueSet/ocp-practitioner-role";
-        static final String CONSENT_CATEGORY_PATH="/ValueSet/ocp-practitioner-role";
-        static final String CONSENT_SECURITY_ROLE_PATH="/ValueSet/ocp-practitioner-role";
-        static final String CONSENT_ACTION_PATH="/ValueSet/ocp-practitioner-role";
-        static final String PURPOSE_OF_USE_PATH="/ValueSet/ocp-practitioner-role";
+        static final String CONSENT_STATE_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/consent-state-codes";
+        static final String CONSENT_CATEGORY_PATH="/ValueSet/consent-category";
+        static final String CONSENT_SECURITY_ROLE_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/security-role-type";
+        static final String CONSENT_ACTION_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/consent-action";
+        static final String PURPOSE_OF_USE_PATH="/ValueSet/$expand?url=http://hl7.org/fhir/ValueSet/v3-ActReason";
     }
 }
