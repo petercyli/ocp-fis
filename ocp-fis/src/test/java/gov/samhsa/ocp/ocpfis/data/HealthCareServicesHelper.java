b839942ed8afa6ae1806122e2b2ec582dd7736f4
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-05-01 10:58:33 -0400

    Create list of dtos from the worksheet for healthcareservices
    19694 - Load test data

--- ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/data/HealthCareServicesHelper.java
+++ ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/data/HealthCareServicesHelper.java
@@ -1,9 +1,21 @@
 package gov.samhsa.ocp.ocpfis.data;
 
+import gov.samhsa.ocp.ocpfis.service.dto.HealthcareServiceDto;
+import gov.samhsa.ocp.ocpfis.service.dto.TelecomDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ValueSetDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+@Slf4j
 public class HealthCareServicesHelper {
 
     public static void process(Sheet healthCareServices, Map<String, String> mapOrganizations) {
@@ -11,7 +23,69 @@ public class HealthCareServicesHelper {
 
         int rowNum = 0;
 
+        Map<String, String> categoryLookups = CommonHelper.getLookup("http://localhost:8444/lookups/healthcare-service-categories");
+        Map<String, String> typeLookups = CommonHelper.getLookup("http://localhost:8444/lookups/healthcare-service-types");
+        Map<String, String> specialityLookups = CommonHelper.getLookup("http://localhost:8444/lookups/healthcare-service-specialities");
+        Map<String, String> referralLookups = CommonHelper.getLookup("http://localhost:8444/lookups/healthcare-service-referral-methods");
 
+        List<HealthcareServiceDto> healthCareServiceDtos = new ArrayList<>();
+
+        for (Row row : healthCareServices) {
+            if (rowNum > 0) {
+                int j = 0;
+                HealthcareServiceDto dto = new HealthcareServiceDto();
+                for (Cell cell : row) {
+                    String cellValue = new DataFormatter().formatCellValue(cell);
+
+                    if (j == 0) {
+                        //organization
+                        dto.setOrganizationId(mapOrganizations.get(cellValue));
+                    } else if (j == 1) {
+                        //name
+                        dto.setName(cellValue);
+                    } else if (j == 2) {
+                        //program name
+                        dto.setProgramName(Arrays.asList(cellValue));
+                    } else if (j == 3) {
+                        //category
+
+                        ValueSetDto valueSetDto = new ValueSetDto();
+                        valueSetDto.setCode(categoryLookups.get(cellValue.trim()));
+                        dto.setCategory(valueSetDto);
+                    } else if (j == 4) {
+                        //type
+
+                        ValueSetDto valueSetDto = new ValueSetDto();
+                        valueSetDto.setCode(typeLookups.get(cellValue.trim()));
+                        dto.setType(Arrays.asList(valueSetDto));
+                    } else if (j == 5) {
+                        //speciality
+
+                        ValueSetDto valueSetDto = new ValueSetDto();
+                        valueSetDto.setCode(specialityLookups.get(cellValue.trim()));
+                        dto.setSpecialty(Arrays.asList(valueSetDto));
+                    } else if (j == 6) {
+                        //referral
+
+                        ValueSetDto valueSetDto = new ValueSetDto();
+                        valueSetDto.setCode(referralLookups.get(cellValue.trim()));
+                        dto.setReferralMethod(Arrays.asList(valueSetDto));
+                    } else if (j == 7) {
+                        //contact
+                        log.info(cellValue);
+                        TelecomDto telecomDto = new TelecomDto();
+                        telecomDto.setValue(Optional.of(cellValue));
+                        dto.setTelecom(Arrays.asList(telecomDto));
+                    }
+
+                    j++;
+                }
+                healthCareServiceDtos.add(dto);
+            }
+            rowNum++;
+        }
+
+        healthCareServiceDtos.forEach(healthcareServiceDto -> log.info("healthcareServiceDto : " + healthcareServiceDto));
     }
 
 }
