bf6f66e4e03a605e2e29d27775e472a8e330654b
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-05-04 17:36:08 -0400

    Create task from excel
    #19694:As a Developer, I will create documentation process of data script

--- ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/data/TasksHelper.java
+++ ocp-fis/src/test/java/gov/samhsa/ocp/ocpfis/data/TasksHelper.java
@@ -1,13 +1,87 @@
 package gov.samhsa.ocp.ocpfis.data;
 
+import gov.samhsa.ocp.ocpfis.data.model.activitydefinition.TempPeriodDto;
+import gov.samhsa.ocp.ocpfis.data.model.task.TempTaskDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ReferenceDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ValueSetDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class TasksHelper {
 
-    public static void process(Sheet locations, Map<String, String> mapOfPractitioners) {
+    public static void process(Sheet tasks, Map<String, String> mapOfPractitioners) {
+        log.info("last row number:" +tasks.getLastRowNum());
 
+        int rowNum=0;
+
+        List<TempTaskDto> taskDtos=new ArrayList<>();
+        Map<String,ValueSetDto> statusLookupValueSet=CommonHelper.getLookupValueSet("http://localhost:8444/lookups/task-status");
+        Map<String,ValueSetDto> priorityValueSet=CommonHelper.getLookupValueSet("http://localhost:8444/lookups/request-priority");
+        Map<String,ValueSetDto> intentValueSet=CommonHelper.getLookupValueSet("http://localhost:8444/lookups/request-intent");
+        Map<String,ValueSetDto> performerValueSet=CommonHelper.getLookupValueSet("http://localhost:8444/lookups/task-performer-type");
+        for(Row row: tasks){
+            if(rowNum>0){
+                int j=0;
+                TempTaskDto dto=new TempTaskDto();
+                TempPeriodDto tempPeriodDto=new TempPeriodDto();
+                for(Cell cell: row){
+                    String cellValue=new DataFormatter().formatCellValue(cell);
+
+                    if(j==0){
+                        ReferenceDto referenceDto=new ReferenceDto();
+                        referenceDto.setReference("Patient/"+CommonHelper.getPatientId(cellValue));
+                        referenceDto.setDisplay(cellValue);
+                        dto.setBeneficiary(referenceDto);
+                    }
+                    else if(j==1){
+                        dto.setDescription(cellValue);
+                    }else if(j==2){
+                        //No organization and task agent(who created task)
+                        // to determine which activity definition to find
+                        //as many organization can have different activity definition of same name.
+                    }else if(j==3){
+                        dto.setStatus(statusLookupValueSet.get(cellValue));
+                    }else if(j==4){
+                        dto.setPriority(priorityValueSet.get(cellValue));
+                    }else if(j==5){
+                        dto.setIntent(intentValueSet.get(cellValue));
+                    }else if(j==6){
+                        ReferenceDto referenceDto=new ReferenceDto();
+                        referenceDto.setReference("Practitioner/"+CommonHelper.getPractitionerId(cellValue));
+                        referenceDto.setDisplay(cellValue);
+                        dto.setOwner(referenceDto);
+                    }else if(j==7){
+                        dto.setPerformerType(performerValueSet.get(cellValue));
+                    }else if(j==8){
+                        tempPeriodDto.setStart(cellValue);
+                    }else if(j==9){
+                        tempPeriodDto.setEnd(cellValue);
+                        dto.setExecutionPeriod(tempPeriodDto);
+                    }
+                    j++;
+                }
+                taskDtos.add(dto);
+            }
+            rowNum++;
+        }
+        RestTemplate rt=new RestTemplate();
+
+        taskDtos.forEach(taskDto->{
+            log.info("related persons : "+taskDto);
+            HttpEntity<TempTaskDto> request=new HttpEntity<>(taskDto);
+            rt.postForObject("http://localhost:8444/related-persons/",request,TempTaskDto.class);
+        });
     }
 
 }
