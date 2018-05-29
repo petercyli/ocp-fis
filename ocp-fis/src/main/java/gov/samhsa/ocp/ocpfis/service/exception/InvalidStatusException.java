a9218e549db5c8bb9afd4c73755107d2d68ce850
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-05-29 09:10:29 -0400

    Fix HttpStatus for the validation error
    18067 - Task state machine

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/exception/InvalidStatusException.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/exception/InvalidStatusException.java
@@ -3,7 +3,7 @@ package gov.samhsa.ocp.ocpfis.service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidStatusException extends RuntimeException {
 
     public InvalidStatusException() {
