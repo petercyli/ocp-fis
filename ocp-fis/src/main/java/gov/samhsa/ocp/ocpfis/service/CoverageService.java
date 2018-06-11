2672e2a797af330da2c4d163816619952067ac58
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-06-11 14:41:57 -0400

    Create a coverage Service
    #20218:As a User I will be able to capture Patient's coverage information - Wireframes and Functionality.

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/CoverageService.java
@@ -0,0 +1,8 @@
+package gov.samhsa.ocp.ocpfis.service;
+
+import gov.samhsa.ocp.ocpfis.service.dto.CoverageDto;
+
+public interface CoverageService {
+
+    void createCoverage(CoverageDto coverageDto);
+}
