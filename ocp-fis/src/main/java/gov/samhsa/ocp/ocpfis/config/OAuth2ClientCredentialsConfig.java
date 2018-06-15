fb11c9d21ffbf4641a91a69cf569a6c09e60b9d6
Shruti Rao <shruti.rao@feisystems.com>
2018-06-15 15:57:18 -0400

    Use oAuth2ClientProperties
    #20212:As a developer, I will design and implement a way for OCP FHIR Integration Service (FIS) to access secured SoF FHIR Server, so that OCP can still functions correctly after secured SoF FHIR Server is introduced

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/config/OAuth2ClientCredentialsConfig.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/config/OAuth2ClientCredentialsConfig.java
@@ -2,6 +2,7 @@ package gov.samhsa.ocp.ocpfis.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,18 +18,12 @@ public class OAuth2ClientCredentialsConfig {
     @Value("${security.oauth2.client.accessTokenUri}")
     private String accessTokenUri;
 
-    @Value("${security.oauth2.client.clientId}")
-    private String clientId;
-
-    @Value("${security.oauth2.client.clientSecret}")
-    private String clientSecret;
-
     @Bean
-    public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
+    public ClientCredentialsResourceDetails clientCredentialsResourceDetails(OAuth2ClientProperties oAuth2ClientProperties) {
         ClientCredentialsResourceDetails clientCredentialsResourceDetails = new ClientCredentialsResourceDetails();
         clientCredentialsResourceDetails.setAccessTokenUri(accessTokenUri);
-        clientCredentialsResourceDetails.setClientId(clientId);
-        clientCredentialsResourceDetails.setClientSecret(clientSecret);
+        clientCredentialsResourceDetails.setClientId(oAuth2ClientProperties.getClientId());
+        clientCredentialsResourceDetails.setClientSecret(oAuth2ClientProperties.getClientSecret());
         return clientCredentialsResourceDetails;
     }
 
@@ -39,7 +34,7 @@ public class OAuth2ClientCredentialsConfig {
     }
 
     @Bean
-    public OAuth2RestTemplate createRestTemplate(OAuth2ClientContext oAuth2ClientContext) {
-        return new OAuth2RestTemplate(clientCredentialsResourceDetails(), oAuth2ClientContext);
+    public OAuth2RestTemplate createRestTemplate(OAuth2ClientContext oAuth2ClientContext, ClientCredentialsResourceDetails resource) {
+        return new OAuth2RestTemplate(resource, oAuth2ClientContext);
     }
 }
