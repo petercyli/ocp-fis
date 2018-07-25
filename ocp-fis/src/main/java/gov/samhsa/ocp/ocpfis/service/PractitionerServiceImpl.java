2d63cf5baf63291ccdca5793aa19ee90e972e675
sadhch <sadhana.chandravanka@feisystems.com>
2018-07-25 02:09:43 -0400

    Temporarily remove the role constraint for practitioner search
    20855 - Create Flag: Author is not populated

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/PractitionerServiceImpl.java
@@ -176,7 +176,7 @@ public class PractitionerServiceImpl implements PractitionerService {
             IQuery iQuery = fhirClient.search().forResource(PractitionerRole.class)
                     .where(new ReferenceClientParam("organization").hasId(organization.get()));
 
-            role.ifPresent(r -> iQuery.where(new TokenClientParam("role").exactly().code(r)));
+            //role.ifPresent(r -> iQuery.where(new TokenClientParam("role").exactly().code(r)));
 
             Bundle bundle = (Bundle) iQuery.include(PractitionerRole.INCLUDE_PRACTITIONER)
                     .sort().descending(PARAM_LASTUPDATED)
@@ -208,7 +208,7 @@ public class PractitionerServiceImpl implements PractitionerService {
             IQuery iQuery = fhirClient.search().forResource(PractitionerRole.class)
                     .where(new ReferenceClientParam("practitioner").hasId(ResourceType.Practitioner + "/" + practitioner.get()));
 
-            role.ifPresent(r -> iQuery.where(new TokenClientParam("role").exactly().code(r)));
+            //role.ifPresent(r -> iQuery.where(new TokenClientParam("role").exactly().code(r)));
 
             Bundle bundle = (Bundle) iQuery.include(PractitionerRole.INCLUDE_ORGANIZATION)
                     .returnBundle(Bundle.class).execute();
