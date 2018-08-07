dbc4c17f6fd025299972001310e6df2fb5ec16fe
Shruti Rao <shruti.rao@feisystems.com>
2018-08-07 14:09:02 -0400

    Format file
    #20668:As a Developer, I will have added Coding system/Codeable Concept reference to all resources that exist in OCP

--- ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/CoverageToCoverageDtoMap.java
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/CoverageToCoverageDtoMap.java
@@ -8,8 +8,8 @@ import org.hl7.fhir.dstu3.model.Coverage;
 import java.util.Optional;
 
 public class CoverageToCoverageDtoMap {
-    public static CoverageDto map(Coverage coverage){
-        CoverageDto coverageDto=new CoverageDto();
+    public static CoverageDto map(Coverage coverage) {
+        CoverageDto coverageDto = new CoverageDto();
         coverageDto.setLogicalId(coverage.getIdElement().getIdPart());
         coverageDto.setStatus(coverage.getStatus().toCode());
         coverageDto.setStatusDisplay(Optional.of(coverage.getStatus().getDisplay()));
@@ -21,7 +21,7 @@ public class CoverageToCoverageDtoMap {
         coverageDto.setSubscriber(FhirDtoUtil.convertReferenceToReferenceDto(coverage.getSubscriber()));
         coverageDto.setSubscriberId(coverage.getSubscriberId());
         coverageDto.setBeneficiary(FhirDtoUtil.convertReferenceToReferenceDto(coverage.getBeneficiary()));
-        coverage.getRelationship().getCoding().stream().findAny().ifPresent(coding->{
+        coverage.getRelationship().getCoding().stream().findAny().ifPresent(coding -> {
             coverageDto.setRelationship(coding.getCode());
             coverageDto.setRelationshipDisplay(Optional.ofNullable(coding.getDisplay()));
         });
