cf84e7d908ae2e4641be43a058a4a3b5cbef963c
Shruti Rao <shruti.rao@feisystems.com>
2018-08-07 12:17:06 -0400

    Add and use EpisodeOfCareConstants
    #20668:As a Developer, I will have added Coding system/Codeable Concept reference to all resources that exist in OCP

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/constants/EpisodeOfCareConstants.java
@@ -0,0 +1,9 @@
+package gov.samhsa.ocp.ocpfis.constants;
+
+public class EpisodeOfCareConstants {
+    public static final String NA = "NA";
+    // PRIVATE //
+    private EpisodeOfCareConstants(){
+        throw new AssertionError();
+    }
+}
