7fd003a968ddd0dff1d7f21af53683fc528056d7
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-08-26 15:23:57 -0400

    Refactor to add two mapper classes to convert dto to Communication and viceversa
    20583 - Communication functionality fixes

---
+++ ocp-fis/src/main/java/gov/samhsa/ocp/ocpfis/service/mapping/CommunicationToCommunicationDtoMap.java
@@ -0,0 +1,123 @@
+package gov.samhsa.ocp.ocpfis.service.mapping;
+
+import gov.samhsa.ocp.ocpfis.service.dto.CommunicationDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ReferenceDto;
+import gov.samhsa.ocp.ocpfis.service.dto.ValueSetDto;
+import gov.samhsa.ocp.ocpfis.service.exception.FHIRClientException;
+import gov.samhsa.ocp.ocpfis.util.DateUtil;
+import gov.samhsa.ocp.ocpfis.util.FhirDtoUtil;
+import org.hl7.fhir.dstu3.model.Communication;
+import org.hl7.fhir.exceptions.FHIRException;
+
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class CommunicationToCommunicationDtoMap {
+
+    public static CommunicationDto map(Communication communication) {
+        CommunicationDto communicationDto = new CommunicationDto();
+
+        if (communication.hasMeta() && communication.getMeta().hasLastUpdated()) {
+            Date lastUpdated = communication.getMeta().getLastUpdated();
+            communicationDto.setLastUpdated(DateUtil.convertDateTimeToString(lastUpdated));
+        }
+
+        communicationDto.setLogicalId(communication.getIdElement().getIdPart());
+
+        communicationDto.setNotDone(communication.getNotDone());
+
+        if (communication.hasStatus()) {
+            communicationDto.setStatusValue(communication.getStatus().getDisplay());
+            communicationDto.setStatusCode(communication.getStatus().toCode());
+        }
+
+        if (communication.hasCategory()) {
+            ValueSetDto category = FhirDtoUtil.convertCodeableConceptListToValuesetDto(communication.getCategory());
+            communicationDto.setCategoryValue(category.getDisplay());
+            communicationDto.setCategoryCode(category.getCode());
+        }
+
+        if (communication.hasMedium()) {
+            ValueSetDto medium = FhirDtoUtil.convertCodeableConceptListToValuesetDto(communication.getMedium());
+            communicationDto.setMediumValue(medium.getDisplay());
+            communicationDto.setMediumCode(medium.getCode());
+        }
+
+        if (communication.hasNotDoneReason()) {
+            ValueSetDto notDoneReason = FhirDtoUtil.convertCodeableConceptToValueSetDto(communication.getNotDoneReason());
+            communicationDto.setNotDoneReasonValue(notDoneReason.getDisplay());
+            communicationDto.setNotDoneReasonCode(notDoneReason.getCode());
+        }
+
+        if (communication.hasRecipient()) {
+            communicationDto.setRecipient(communication.getRecipient().stream().map(FhirDtoUtil::convertReferenceToReferenceDto).collect(Collectors.toList()));
+        }
+
+        if (communication.hasSender()) {
+            communicationDto.setSender(ReferenceDto.builder()
+                    .reference((communication.getSender().getReference() != null && !communication.getSender().getReference().isEmpty()) ? communication.getSender().getReference() : null)
+                    .display((communication.getSender().getDisplay() != null && !communication.getSender().getDisplay().isEmpty()) ? communication.getSender().getDisplay() : null)
+                    .build());
+        }
+
+        if (communication.hasSubject()) {
+            communicationDto.setSubject(ReferenceDto.builder()
+                    .reference((communication.getSubject().getReference() != null && !communication.getSubject().getReference().isEmpty()) ? communication.getSubject().getReference() : null)
+                    .display((communication.getSubject().getDisplay() != null && !communication.getSubject().getDisplay().isEmpty()) ? communication.getSubject().getDisplay() : null)
+                    .build());
+        }
+
+
+        if (communication.hasTopic()) {
+            communicationDto.setTopic(ReferenceDto.builder()
+                    .reference(FhirDtoUtil.convertReferenceToReferenceDto(communication.getTopic().stream().findAny().get()).getReference())
+                    .display(FhirDtoUtil.convertReferenceToReferenceDto(communication.getTopic().stream().findAny().get()).getDisplay())
+                    .build());
+        }
+
+
+        if (communication.hasDefinition()) {
+            communicationDto.setDefinition(ReferenceDto.builder()
+                    .reference(FhirDtoUtil.convertReferenceToReferenceDto(communication.getDefinition().stream().findAny().get()).getReference())
+                    .display(FhirDtoUtil.convertReferenceToReferenceDto(communication.getDefinition().stream().findAny().get()).getDisplay())
+                    .build());
+        }
+
+        //TODO: Remove this after data purge.
+        if (communication.hasContext()) {
+            communicationDto.setContext(ReferenceDto.builder()
+                    .reference((communication.getContext().getReference() != null && !communication.getContext().getReference().isEmpty()) ? communication.getContext().getReference() : null)
+                    .display((communication.getContext().getDisplay() != null && !communication.getContext().getDisplay().isEmpty()) ? communication.getContext().getDisplay() : null)
+                    .build());
+        }
+
+
+        if (communication.hasNote()) {
+            communicationDto.setNote(communication.getNote().stream().findAny().get().getText());
+        }
+
+
+        if (communication.hasNote()) {
+            communicationDto.setNote(communication.getNote().stream().findAny().get().getText());
+        }
+
+        if (communication.hasPayload()) {
+            try {
+                communicationDto.setPayloadContent(communication.getPayload().stream().findAny().get().getContentStringType().getValue());
+            } catch (FHIRException e) {
+                throw new FHIRClientException("FHIR Client returned with an error while load a communication:" + e.getMessage());
+            }
+        }
+
+        if (communication.hasSent()) {
+            communicationDto.setSent(DateUtil.convertDateTimeToString(communication.getSent()));
+        }
+
+        if (communication.hasReceived()) {
+            communicationDto.setReceived(DateUtil.convertDateTimeToString(communication.getReceived()));
+        }
+
+        return communicationDto;
+
+    }
+}
